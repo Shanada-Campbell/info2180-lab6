8164c34bb4372a3b49ac7455fa0df5e7ef2ce20f
Shanada Campbell <shanada.campbell@mymona.uwi.edu>
2018-11-13 00:01:30 +0000

    Exercise 3

--- application.js
+++ application.js
@@ -1,15 +1,19 @@
 // JavaScript File
 
 window.onload = function() {
-    let search = document.getElementById("button");
+    let search = document.getElementById("search");
+    let result = document.getElementById("result");
     search.addEventListener( "click" , function(){
         let hRequest = XMLHttpRequest();
-        hRequest.onreadystagechange = function() {
+        hRequest.onreadystatechange = function() {
             if (hRequest.readyState == 4 && hRequest.status == 200) {
-                alert(hRequest.responseText);
+                let reply = document.createElement();
+                reply.innerHTML = hRequest.responseText;
+                result.appendChild(reply);
             }
         };
-        hRequest.open("GET","request.php?q=definition",true);
+        let url = "request.php?q="+document.getElementById("search_value").value;
+        hRequest.open("GET", url, true);
         hRequest.send();
     });
 };
